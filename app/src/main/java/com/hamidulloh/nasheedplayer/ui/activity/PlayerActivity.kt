0486b1b517e090e2c67419a83c280d289849b2a0
UsmonHamidulloh <usman.x.0221@gmail.com>
2021-04-06 15:16:12 +0500

    seekBar is working

--- app/src/main/java/com/hamidulloh/nasheedplayer/ui/activity/PlayerActivity.kt
+++ app/src/main/java/com/hamidulloh/nasheedplayer/ui/activity/PlayerActivity.kt
@@ -3,6 +3,9 @@ package com.hamidulloh.nasheedplayer.ui.activity
 import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import com.hamidulloh.nasheedplayer.R
 import com.hamidulloh.nasheedplayer.databinding.ActivityPlayerBinding
@@ -63,6 +66,60 @@ class PlayerActivity : AppCompatActivity() {
 
         }
 
+        binding.positionBar.max = mediaPlayer.duration
+
+        binding.positionBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress)
+                    mediaPlayer.start()
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        Thread(Runnable {
+            while (mediaPlayer != null) {
+                try {
+                    val msg = Message()
+                    msg.what = mediaPlayer.currentPosition
+                    handler.sendMessage(msg)
+                    Thread.sleep(1_000)
+                } catch (e: InterruptedException) {
+
+                }
+            }
+        }).start()
+    }
+
+    var handler = @SuppressLint("HandlerLeak")
+    object : Handler() {
+        override fun handleMessage(msg: Message) {
+            val currentPosition = msg.what
+            val elapsedTime = createTimeLabel(currentPosition)
+
+            binding.positionBar.progress = currentPosition
+            binding.firstTime.text = elapsedTime
+        }
+    }
+
+    private fun createTimeLabel(time: Int): String {
+        var timeLabel = ""
+        val min = time / 1000 / 60
+        val sec = time / 1000 % 60
+
+        timeLabel = "$min:"
+        if (sec < 10) timeLabel += "0"
+        timeLabel += sec
+
+        return timeLabel
     }
 
     private fun calculateTime(duration: Int) {
